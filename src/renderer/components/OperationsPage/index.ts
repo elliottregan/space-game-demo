cc3c6eac20c759a9e379ebc4cd18f9abc3dcad56
Elliott Regan <950202+elliottregan@users.noreply.github.com>
2026-01-30 18:29:56 +0000

    feat(operations): add barrel export

---
+++ src/renderer/components/OperationsPage/index.ts
@@ -0,0 +1,6 @@
+export { default as OperationsPage } from "./OperationsPage.vue";
+export { default as ColonistPool } from "./ColonistPool.vue";
+export { default as ColonistDragCard } from "./ColonistDragCard.vue";
+export { default as BuildingRoleGroup } from "./BuildingRoleGroup.vue";
+export { default as BuildingWorkSlots } from "./BuildingWorkSlots.vue";
+export { default as WorkerSlot } from "./WorkerSlot.vue";
